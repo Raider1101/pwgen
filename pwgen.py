8aeebc84d186532433401854b10a3aadc04fc1ca
Raider1101 <raider1101@protonmail.com>
2023-06-28 09:12:07 -0300

    memory error fixed

--- pwgen.py
+++ pwgen.py
@@ -2,17 +2,13 @@ import itertools
 
 def generate_combinations(num_characters):
     characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%&*"
-    
-    combinations = []
-    for i in range(1, num_characters + 1):
-        combinations.extend(itertools.product(characters, repeat=i))
-    return combinations
 
-def save_combinations(combinations):
     with open("combinations.txt", "w") as file:
-        for combination in combinations:
-            line = ''.join(combination) + "\n"
-            file.write(line)
+        for i in range(1, num_characters + 1):
+            combinations = itertools.product(characters, repeat=i)
+            for combination in combinations:
+                line = ''.join(combination) + "\n"
+                file.write(line)
 
 def print_banner():
     banner = '''
@@ -37,8 +33,7 @@ def print_banner():
 def main():
     print_banner()
     num_characters = int(input("Enter the maximum number of characters per combination: "))
-    combinations = generate_combinations(num_characters)
-    save_combinations(combinations)
+    generate_combinations(num_characters)
     print("The combinations have been saved in the file combinations.txt.")
 
 if __name__ == "__main__":
